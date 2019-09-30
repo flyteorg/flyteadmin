62725732b38e1931374e2fd7565880a4c2d4ccdf
Miguel Toledo <mtoledo@lyft.com>
2019-09-29 18:37:40 -0700

    to lowercase

--- pkg/runtime/configuration_provider.go
+++ pkg/runtime/configuration_provider.go
@@ -57,10 +57,6 @@ func NewConfigurationProvider() interfaces.Configuration {
 		whitelistConfiguration:              NewWhitelistConfigurationProvider(),
 		registrationValidationConfiguration: NewRegistrationValidationProvider(),
 		clusterResourceConfiguration:        NewClusterResourceConfigurationProvider(),
-<<<<<<< HEAD
-=======
 		namespaceMappingConfiguration:       NewNamespaceMappingConfigurationProvider(),
-		propellerExecutorConfiguration:      NewPropellerExecutorConfigurationProvider(),
->>>>>>> update propeller executor config
 	}
 }
