fd17fb7385297cd4784b60f320b442851fb6586d
Yee Hing Tong <ytong@lyft.com>
2019-10-06 17:15:06 -0700

    switched out the jwt-go library for the go-oidc library, which is also capable of handling jwt verification, and is even better because it caches jwks key sets.

--- pkg/auth/token.go
+++ pkg/auth/token.go
@@ -2,84 +2,27 @@ package auth
 
 import (
 	"context"
-	"encoding/json"
-	"github.com/dgrijalva/jwt-go"
-	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/coreos/go-oidc"
 	"github.com/lyft/flytestdlib/errors"
 	"github.com/lyft/flytestdlib/logger"
 	"golang.org/x/oauth2"
+	"strings"
 	"time"
 )
-
-const (
-	ErrJwtVerification errors.ErrorCode = "JWKS_FAILURE"
-)
-
-type JwtVerifier struct {
-	Url string
-}
-
-func (j *JwtVerifier) GetKey(token *jwt.Token) (interface{}, error) {
-	set, err := jwk.FetchHTTP(j.Url)
-	if err != nil {
-		return nil, err
-	}
-
-	keyID, ok := token.Header["kid"].(string)
-	if !ok {
-		return nil, errors.Errorf(ErrJwtVerification, "JWT header is missing a kid")
-	}
-
-	if key := set.LookupKeyID(keyID); len(key) == 1 {
-		return key[0].Materialize()
-	}
-
-	return nil, errors.Errorf(ErrJwtVerification, "unable to find key")
-}
 
 const (
 	ErrRefreshingToken errors.ErrorCode = "TOKEN_REFRESH_FAILURE"
 	ErrTokenExpired                     = "JWT_EXPIRED"
 	ErrJwtValidation                    = "JWT_VERIFICATION_FAILED"
-	ErrNilJwt                           = "NIL_JWT_ERROR"
-	ErrEmptySub                         = "SUB_EMPTY"
 )
 
-func getExpiration(claims jwt.MapClaims) *time.Time {
-	switch exp := claims["exp"].(type) {
-	case float64:
-		t := time.Unix(int64(exp), 0)
-		return &t
-	case json.Number:
-		v, _ := exp.Int64()
-		t := time.Unix(v, 0)
-		return &t
-	default:
-		return nil
-	}
-}
-
-// Refreshes a JWT.
-// NB: The input token and the output token are from two different libraries
-func GetRefreshedToken(ctx context.Context, oauth oauth2.Config, token jwt.Token, refreshToken string) (*oauth2.Token, error) {
+// Refresh a JWT
+func GetRefreshedToken(ctx context.Context, oauth oauth2.Config, accessToken, refreshToken string) (*oauth2.Token, error) {
 	logger.Debugf(ctx, "Attempting to refresh token")
-	claims := token.Claims.(jwt.MapClaims)
-	expiration := getExpiration(claims)
-	var originalToken oauth2.Token
-	if expiration == nil {
-		logger.Infof(ctx, "No expiration claim found")
-		originalToken = oauth2.Token{
-			AccessToken:  token.Raw,
-			RefreshToken: refreshToken,
-			Expiry:       time.Now().Add(-1 * time.Minute), // force expired by setting to the past
-		}
-	} else {
-		logger.Infof(ctx, "Refreshing access token with expiration %s", expiration)
-		originalToken = oauth2.Token{
-			AccessToken:  token.Raw,
-			RefreshToken: refreshToken,
-			Expiry:       *expiration,
-		}
+	originalToken := oauth2.Token{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+		Expiry:       time.Now().Add(-1 * time.Minute), // force expired by setting to the past
 	}
 
 	tokenSource := oauth.TokenSource(ctx, &originalToken)
@@ -92,39 +35,20 @@ func GetRefreshedToken(ctx context.Context, oauth oauth2.Config, token jwt.Token
 	return newToken, nil
 }
 
-func constructMapClaims(claims Claims) jwt.MapClaims {
-	// TODO: iterate
-	return jwt.MapClaims{
-		Aud: claims.Audience,
-		Iss: claims.Issuer,
-	}
-}
-
 func ParseAndValidate(ctx context.Context, options OAuthOptions, accessToken string,
-	keyFunc jwt.Keyfunc) (*jwt.Token, error) {
+	provider *oidc.Provider) (*oidc.IDToken, error) {
 
-	t, err := jwt.ParseWithClaims(accessToken, constructMapClaims(options.Claims), keyFunc)
+	var verifier = provider.Verifier(&oidc.Config{ClientID: options.Claims.Audience})
+
+	idToken, err := verifier.Verify(ctx, accessToken)
 	if err != nil {
 		logger.Debugf(ctx, "JWT parsing with claims failed %s", err)
 		flyteErr := errors.Wrapf(ErrJwtValidation, err, "jwt parse with claims failed")
-		if validationErr, ok := err.(*jwt.ValidationError); ok &&
-			validationErr.Errors == jwt.ValidationErrorExpired && t != nil {
-			return t, errors.Wrapf(ErrTokenExpired, flyteErr, "token is expired")
+		// TODO: Contribute an errors package to the go-oidc library for proper error handling
+		if strings.Contains(err.Error(), "token is expired") {
+			return idToken, errors.Wrapf(ErrTokenExpired, flyteErr, "token is expired")
 		}
-		return t, flyteErr
+		return idToken, flyteErr
 	}
-
-	return t, nil
-}
-
-func GetSubClaim(t *jwt.Token) (string, error) {
-	claims := t.Claims.(jwt.MapClaims)
-	if userEmail, ok := claims["sub"]; ok {
-		if userEmail.(string) != "" {
-			return userEmail.(string), nil
-		} else {
-			return "", errors.Errorf(ErrEmptySub, "Email not found in sub claim")
-		}
-	}
-	return "", errors.Errorf(ErrEmptySub, "No sub claim found")
+	return idToken, nil
 }
--- pkg/auth/token_test.go
+++ pkg/auth/token_test.go
@@ -1,19 +1,64 @@
 package auth
 
 import (
-	"github.com/dgrijalva/jwt-go"
+	"context"
+	"fmt"
+	"github.com/coreos/go-oidc"
 	"github.com/stretchr/testify/assert"
+	"net/http"
+	"strings"
 	"testing"
 )
 
-func TestParse(t *testing.T) {
-	jwtVerifier := JwtVerifier{
-		Url: "https://lyft.okta.com/oauth2/default/v1/keys",
+//func TestParse(t *testing.T) {
+//	jwtVerifier := JwtVerifier{
+//		Url: "https://lyft.okta.com/oauth2/default/v1/keys",
+//	}
+//	tokenStr := "eyJraWQiOiItY2FQXzgyX1o0cVVDMnUtWkRPS2pPYVVIa2RkaWN3YUJOMGJjMTZIb3ZFIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULndRZDFab0dxS3RJa3JGY3pmTlRkd1JnZmZZM0VDSjU1VmNPZDVjQmxEMncuQ05mcGRMVlNoem4wekdWZEp0T1kyL080TXcrM0pRc1BxSGFBSms5bkxQRT0iLCJpc3MiOiJodHRwczovL2x5ZnQub2t0YS5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiJhcGk6Ly9kZWZhdWx0IiwiaWF0IjoxNTY5MjgyODYzLCJleHAiOjE1NjkyODY0NjMsImNpZCI6IjBvYWJzNTJna3VYNFZ6WXRKMXQ3IiwidWlkIjoiMDB1MXAyMjB2NmRtc0N3Y28xdDciLCJzY3AiOlsib2ZmbGluZV9hY2Nlc3MiLCJvcGVuaWQiXSwic3ViIjoieXRvbmdAbHlmdC5jb20ifQ.pVSnEBcATN6mE9PEa6Ht7Sd-EjzM3Cm6yxR-_pMWzo9DlmVFcV_T8WsNq2pdboblfuZXa-53a0aajrSXbWa-HkHOiE0g0RZaVBZdkp3Wtqj7Xnh9ROhvjLu8V42NcJBpvZlksjAepdoexaVHiIxcxM7lJf49ZiIrvE7L2ybuKYUE5iaZoM47jfizxUcRV-r_GriE0UtUoJRwioGKTm9bpPCA4odM7et42D1zd8bFs32so1cMJcSIHmvDIO5vZMYER29z1mZf-EKr9GrKSrUdjxDncnMspNt1BwwaI-hEf63csYOgxtmcgyTOjNpnIYgi0RB8G8UjDzn7qYyWbg4hhA"
+//	_, err := jwt.Parse(tokenStr, jwtVerifier.GetKey)
+//	assert.NotNil(t, err)
+//	validationErr, ok := err.(*jwt.ValidationError)
+//	assert.True(t, ok)
+//	assert.Equal(t, jwt.ValidationErrorExpired, validationErr.Errors)
+//}
+
+func TestOidc(t *testing.T) {
+	//tokenStr := "eyJraWQiOiItY2FQXzgyX1o0cVVDMnUtWkRPS2pPYVVIa2RkaWN3YUJOMGJjMTZIb3ZFIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULmRObmh1WTZPMm80QU1sOGNmYVRTWkNqQl9jNW1hYUNZUFZVdzRDZzJOTGcuWlFxMG5Tb0ZDTWl6RUlRUTNlL3gyaXoyY1EwNDdJemgrTDE5UTQ2Q3U4OD0iLCJpc3MiOiJodHRwczovL2x5ZnQub2t0YS5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiJhcGk6Ly9kZWZhdWx0IiwiaWF0IjoxNTcwNDAxNTU3LCJleHAiOjE1NzA0MDUxNTcsImNpZCI6IjBvYWJzNTJna3VYNFZ6WXRKMXQ3IiwidWlkIjoiMDB1MXAyMjB2NmRtc0N3Y28xdDciLCJzY3AiOlsib3BlbmlkIiwib2ZmbGluZV9hY2Nlc3MiXSwic3ViIjoieXRvbmdAbHlmdC5jb20ifQ.iTAwHhWZgdu-k6am8QH85Kt2l5duEiKuex5d2l0uAhppvVBKmyL1n2r55xxea5Q9AtQo_dFtUnceqmi6XVlksppZ__NAAG8v6vHzrUpin1G2XW4Ycv2_HMNSCAZCVQ_JCGIX9INxTb1K43sknZo0eMpEaf4Do24MtkJxQEZpCrPyt_qsMVxyRJBIsUcW3wzd8mMyvn5rX-EWIvDuaQYwrW2egCdw6I2IzWjZ923F9S-neHKkuf3E4fVnp8WUYoWs3BRR9LzZQPwDCES10sixLb7v6khopP4bW2L5yooccDp0Sdied08aFw63Uu4vzFRwIu_vEzEhSaDY7KOgccJjcA"
+	tokenStr := "eyJraWQiOiItY2FQXzgyX1o0cVVDMnUtWkRPS2pPYVVIa2RkaWN3YUJOMGJjMTZIb3ZFIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULnA1WUFlbzBodkhVODdBWGRqTVBDNXRrUG9fdHJBMnk3Z2hLQUMxQ1ktZWcucFNRZGJTR3RUK3hUV1Jza05ZaVd1L2FPSmEvUjd4OTRlbmpWb3dGL2c2QT0iLCJpc3MiOiJodHRwczovL2x5ZnQub2t0YS5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiJhcGk6Ly9kZWZhdWx0IiwiaWF0IjoxNTcwNDA2NjU4LCJleHAiOjE1NzA0MTAyNTgsImNpZCI6IjBvYWJzNTJna3VYNFZ6WXRKMXQ3IiwidWlkIjoiMDB1MXAyMjB2NmRtc0N3Y28xdDciLCJzY3AiOlsib2ZmbGluZV9hY2Nlc3MiLCJvcGVuaWQiXSwic3ViIjoieXRvbmdAbHlmdC5jb20ifQ.VJtWuP4vZN50SMJ6VT4kU7u6YfthgTTSs4KPXj80KpXA5VLt9mXVmlGOoVEVUvJqmgZuh5xqHU0epJw7fgJCH7fBvqVaxmfWleaCJKeZ2lo9Ly5kE_RbxYGrhDu285FOmJzZd_p8zJ1KJRNy5zXbkMCOuRXSqFQol2INXcy7auyxFe4_pN6dMpsz5mbKghBsPPsyRmWBnpuWMIz8HmVZXy6CVh4ik2vSIFVdn9R6ulysSSxwgfZYNHi8NUn_Wbf4lk1VDKNlatgKpwDD4vRYzzE5vu0JdVNKt_TgKc7_VR4ZxUrf82lztHSNmAsnGg5hcCHGWKQKHtcprsI3OSYZDA"
+
+	myClient := &http.Client{}
+	ctx := oidc.ClientContext(context.Background(), myClient)
+
+	//keySet := oidc.NewRemoteKeySet(ctx, "https://lyft.okta.com/oauth2/default/v1/keys")
+
+	provider, err := oidc.NewProvider(ctx, "https://lyft.okta.com/oauth2/default")
+	if err != nil {
+		panic(err)
 	}
-	tokenStr := "eyJraWQiOiItY2FQXzgyX1o0cVVDMnUtWkRPS2pPYVVIa2RkaWN3YUJOMGJjMTZIb3ZFIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULndRZDFab0dxS3RJa3JGY3pmTlRkd1JnZmZZM0VDSjU1VmNPZDVjQmxEMncuQ05mcGRMVlNoem4wekdWZEp0T1kyL080TXcrM0pRc1BxSGFBSms5bkxQRT0iLCJpc3MiOiJodHRwczovL2x5ZnQub2t0YS5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiJhcGk6Ly9kZWZhdWx0IiwiaWF0IjoxNTY5MjgyODYzLCJleHAiOjE1NjkyODY0NjMsImNpZCI6IjBvYWJzNTJna3VYNFZ6WXRKMXQ3IiwidWlkIjoiMDB1MXAyMjB2NmRtc0N3Y28xdDciLCJzY3AiOlsib2ZmbGluZV9hY2Nlc3MiLCJvcGVuaWQiXSwic3ViIjoieXRvbmdAbHlmdC5jb20ifQ.pVSnEBcATN6mE9PEa6Ht7Sd-EjzM3Cm6yxR-_pMWzo9DlmVFcV_T8WsNq2pdboblfuZXa-53a0aajrSXbWa-HkHOiE0g0RZaVBZdkp3Wtqj7Xnh9ROhvjLu8V42NcJBpvZlksjAepdoexaVHiIxcxM7lJf49ZiIrvE7L2ybuKYUE5iaZoM47jfizxUcRV-r_GriE0UtUoJRwioGKTm9bpPCA4odM7et42D1zd8bFs32so1cMJcSIHmvDIO5vZMYER29z1mZf-EKr9GrKSrUdjxDncnMspNt1BwwaI-hEf63csYOgxtmcgyTOjNpnIYgi0RB8G8UjDzn7qYyWbg4hhA"
-	_, err := jwt.Parse(tokenStr, jwtVerifier.GetKey)
-	assert.NotNil(t, err)
-	validationErr, ok := err.(*jwt.ValidationError)
-	assert.True(t, ok)
-	assert.Equal(t, jwt.ValidationErrorExpired, validationErr.Errors)
+	var verifier = provider.Verifier(&oidc.Config{ClientID: "api://default"})
+
+	x, err := verifier.Verify(context.Background(), tokenStr)
+	assert.NoError(t, err)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+}
+
+// Until the go-oidc library uses typed errors, we are left to check expiration with a string match.
+// This test ensures that.
+func TestExpiredToken(t *testing.T) {
+	expiredToken := "eyJraWQiOiItY2FQXzgyX1o0cVVDMnUtWkRPS2pPYVVIa2RkaWN3YUJOMGJjMTZIb3ZFIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULmRObmh1WTZPMm80QU1sOGNmYVRTWkNqQl9jNW1hYUNZUFZVdzRDZzJOTGcuWlFxMG5Tb0ZDTWl6RUlRUTNlL3gyaXoyY1EwNDdJemgrTDE5UTQ2Q3U4OD0iLCJpc3MiOiJodHRwczovL2x5ZnQub2t0YS5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiJhcGk6Ly9kZWZhdWx0IiwiaWF0IjoxNTcwNDAxNTU3LCJleHAiOjE1NzA0MDUxNTcsImNpZCI6IjBvYWJzNTJna3VYNFZ6WXRKMXQ3IiwidWlkIjoiMDB1MXAyMjB2NmRtc0N3Y28xdDciLCJzY3AiOlsib3BlbmlkIiwib2ZmbGluZV9hY2Nlc3MiXSwic3ViIjoieXRvbmdAbHlmdC5jb20ifQ.iTAwHhWZgdu-k6am8QH85Kt2l5duEiKuex5d2l0uAhppvVBKmyL1n2r55xxea5Q9AtQo_dFtUnceqmi6XVlksppZ__NAAG8v6vHzrUpin1G2XW4Ycv2_HMNSCAZCVQ_JCGIX9INxTb1K43sknZo0eMpEaf4Do24MtkJxQEZpCrPyt_qsMVxyRJBIsUcW3wzd8mMyvn5rX-EWIvDuaQYwrW2egCdw6I2IzWjZ923F9S-neHKkuf3E4fVnp8WUYoWs3BRR9LzZQPwDCES10sixLb7v6khopP4bW2L5yooccDp0Sdied08aFw63Uu4vzFRwIu_vEzEhSaDY7KOgccJjcA"
+
+	myClient := &http.Client{}
+	ctx := oidc.ClientContext(context.Background(), myClient)
+	provider, err := oidc.NewProvider(ctx, "https://lyft.okta.com/oauth2/default")
+	if err != nil {
+		panic(err)
+	}
+	var verifier = provider.Verifier(&oidc.Config{ClientID: "api://default"})
+
+	x, err := verifier.Verify(context.Background(), expiredToken)
+	assert.Nil(t, x)
+	assert.Error(t, err)
+	assert.True(t, strings.Contains(err.Error(), "token is expired"))
 }
