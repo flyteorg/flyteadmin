c1f1fa9b4ca279d70e45ddf465a96e15932a0713
Matthew Smith <matthewsmith@lyft.com>
2019-10-29 18:16:06 -0700

    Add more template strings for email notifications

--- pkg/async/notifications/email.go
+++ pkg/async/notifications/email.go
@@ -5,7 +5,6 @@ import (
 
 	"strings"
 
-	"github.com/lyft/flyteadmin/pkg/repositories/models"
 	runtimeInterfaces "github.com/lyft/flyteadmin/pkg/runtime/interfaces"
 	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/admin"
 )
@@ -18,14 +17,30 @@ const domain = "domain"
 const name = "name"
 const phase = "phase"
 const errorPlaceholder = "error"
+const workflowProject = "workflow.project"
+const workflowDomain = "workflow.domain"
+const workflowName = "workflow.name"
+const workflowVersion = "workflow.version"
+const launchPlanProject = "launch_plan.project"
+const launchPlanDomain = "launch_plan.domain"
+const launchPlanName = "launch_plan.name"
+const launchPlanVersion = "launch_plan.version"
 const replaceAllInstances = -1
 
-func substituteEmailParameters(message string, request admin.WorkflowExecutionEventRequest, execution models.Execution) string {
-	response := strings.Replace(message, fmt.Sprintf(substitutionParam, project), execution.Project, replaceAllInstances)
-	response = strings.Replace(response, fmt.Sprintf(substitutionParam, domain), execution.Domain, replaceAllInstances)
-	response = strings.Replace(response, fmt.Sprintf(substitutionParam, name), execution.Name, replaceAllInstances)
+func substituteEmailParameters(message string, request admin.WorkflowExecutionEventRequest, execution *admin.Execution) string {
+	response := strings.Replace(message, fmt.Sprintf(substitutionParam, project), execution.Id.Project, replaceAllInstances)
+	response = strings.Replace(response, fmt.Sprintf(substitutionParam, domain), execution.Id.Domain, replaceAllInstances)
+	response = strings.Replace(response, fmt.Sprintf(substitutionParam, name), execution.Id.Name, replaceAllInstances)
 	response = strings.Replace(response, fmt.Sprintf(substitutionParam, phase),
 		strings.ToLower(request.Event.Phase.String()), replaceAllInstances)
+	response = strings.Replace(response, fmt.Sprintf(substitutionParam, workflowProject), execution.Closure.WorkflowId.Project, replaceAllInstances)
+	response = strings.Replace(response, fmt.Sprintf(substitutionParam, workflowDomain), execution.Closure.WorkflowId.Domain, replaceAllInstances)
+	response = strings.Replace(response, fmt.Sprintf(substitutionParam, workflowName), execution.Closure.WorkflowId.Name, replaceAllInstances)
+	response = strings.Replace(response, fmt.Sprintf(substitutionParam, workflowVersion), execution.Closure.WorkflowId.Version, replaceAllInstances)
+	response = strings.Replace(response, fmt.Sprintf(substitutionParam, launchPlanProject), execution.Spec.LaunchPlan.Project, replaceAllInstances)
+	response = strings.Replace(response, fmt.Sprintf(substitutionParam, launchPlanDomain), execution.Spec.LaunchPlan.Domain, replaceAllInstances)
+	response = strings.Replace(response, fmt.Sprintf(substitutionParam, launchPlanName), execution.Spec.LaunchPlan.Name, replaceAllInstances)
+	response = strings.Replace(response, fmt.Sprintf(substitutionParam, launchPlanVersion), execution.Spec.LaunchPlan.Version, replaceAllInstances)
 	if request.Event.GetError() != nil {
 		response = strings.Replace(response, fmt.Sprintf(substitutionParam, errorPlaceholder),
 			fmt.Sprintf(executionError, request.Event.GetError().Message), replaceAllInstances)
@@ -43,7 +58,7 @@ func ToEmailMessageFromWorkflowExecutionEvent(
 	config runtimeInterfaces.NotificationsConfig,
 	emailNotification admin.EmailNotification,
 	request admin.WorkflowExecutionEventRequest,
-	execution models.Execution) *admin.EmailMessage {
+	execution *admin.Execution) *admin.EmailMessage {
 
 	return &admin.EmailMessage{
 		SubjectLine:     substituteEmailParameters(config.NotificationsEmailerConfig.Subject, request, execution),
--- pkg/async/notifications/email_test.go
+++ pkg/async/notifications/email_test.go
@@ -4,14 +4,52 @@ import (
 	"fmt"
 	"testing"
 
+	"strings"
+
 	"github.com/gogo/protobuf/proto"
-	"github.com/lyft/flyteadmin/pkg/repositories/models"
 	runtimeInterfaces "github.com/lyft/flyteadmin/pkg/runtime/interfaces"
 	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/admin"
 	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/core"
 	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/event"
 	"github.com/stretchr/testify/assert"
 )
+
+const executionProjectValue = "proj"
+const executionDomainValue = "prod"
+const executionNameValue = "e124"
+const launchPlanProjectValue = "lp_proj"
+const launchPlanDomainValue = "lp_domain"
+const launchPlanNameValue = "lp_name"
+const launchPlanVersionValue = "lp_version"
+const workflowProjectValue = "wf_proj"
+const workflowDomainValue = "wf_domain"
+const workflowNameValue = "wf_name"
+const workflowVersionValue = "wf_version"
+
+var workflowExecution = &admin.Execution{
+	Id: &core.WorkflowExecutionIdentifier{
+		Project: executionProjectValue,
+		Domain:  executionDomainValue,
+		Name:    executionNameValue,
+	},
+	Spec: &admin.ExecutionSpec{
+		LaunchPlan: &core.Identifier{
+			Project: launchPlanProjectValue,
+			Domain:  launchPlanDomainValue,
+			Name:    launchPlanNameValue,
+			Version: launchPlanVersionValue,
+		},
+	},
+	Closure: &admin.ExecutionClosure{
+		WorkflowId: &core.Identifier{
+			Project: workflowProjectValue,
+			Domain:  workflowDomainValue,
+			Name:    workflowNameValue,
+			Version: workflowVersionValue,
+		},
+		Phase: core.WorkflowExecution_SUCCEEDED,
+	},
+}
 
 func TestSubstituteEmailParameters(t *testing.T) {
 	message := "{{ unused }}. {{project }} and {{ domain }} and {{ name }} ended up in {{ phase }}.{{ error }}"
@@ -20,22 +58,44 @@ func TestSubstituteEmailParameters(t *testing.T) {
 			Phase: core.WorkflowExecution_SUCCEEDED,
 		},
 	}
-	model := models.Execution{
-		ExecutionKey: models.ExecutionKey{
-			Project: "proj",
-			Domain:  "prod",
-			Name:    "e124",
-		},
-	}
 	assert.Equal(t, "{{ unused }}. {{project }} and prod and e124 ended up in succeeded.",
-		substituteEmailParameters(message, request, model))
+		substituteEmailParameters(message, request, workflowExecution))
 	request.Event.OutputResult = &event.WorkflowExecutionEvent_Error{
 		Error: &core.ExecutionError{
 			Message: "uh-oh",
 		},
 	}
 	assert.Equal(t, "{{ unused }}. {{project }} and prod and e124 ended up in succeeded. The execution failed with error: [uh-oh].",
-		substituteEmailParameters(message, request, model))
+		substituteEmailParameters(message, request, workflowExecution))
+}
+
+func TestSubstituteAllTemplates(t *testing.T) {
+	templateVars := map[string]string{
+		fmt.Sprintf(substitutionParam, project):           executionProjectValue,
+		fmt.Sprintf(substitutionParam, domain):            executionDomainValue,
+		fmt.Sprintf(substitutionParam, name):              executionNameValue,
+		fmt.Sprintf(substitutionParam, launchPlanProject): launchPlanProjectValue,
+		fmt.Sprintf(substitutionParam, launchPlanDomain):  launchPlanDomainValue,
+		fmt.Sprintf(substitutionParam, launchPlanName):    launchPlanNameValue,
+		fmt.Sprintf(substitutionParam, launchPlanVersion): launchPlanVersionValue,
+		fmt.Sprintf(substitutionParam, workflowProject):   workflowProjectValue,
+		fmt.Sprintf(substitutionParam, workflowDomain):    workflowDomainValue,
+		fmt.Sprintf(substitutionParam, workflowName):      workflowNameValue,
+		fmt.Sprintf(substitutionParam, workflowVersion):   workflowVersionValue,
+		fmt.Sprintf(substitutionParam, phase):             strings.ToLower(core.WorkflowExecution_SUCCEEDED.String()),
+	}
+	var messageTemplate, desiredResult []string
+	for template, result := range templateVars {
+		messageTemplate = append(messageTemplate, template)
+		desiredResult = append(desiredResult, result)
+	}
+	request := admin.WorkflowExecutionEventRequest{
+		Event: &event.WorkflowExecutionEvent{
+			Phase: core.WorkflowExecution_SUCCEEDED,
+		},
+	}
+	assert.Equal(t, strings.Join(desiredResult, ","),
+		substituteEmailParameters(strings.Join(messageTemplate, ","), request, workflowExecution))
 }
 
 func TestToEmailMessageFromWorkflowExecutionEvent(t *testing.T) {
@@ -58,14 +118,7 @@ func TestToEmailMessageFromWorkflowExecutionEvent(t *testing.T) {
 			Phase: core.WorkflowExecution_ABORTED,
 		},
 	}
-	model := models.Execution{
-		ExecutionKey: models.ExecutionKey{
-			Project: "proj",
-			Domain:  "prod",
-			Name:    "e124",
-		},
-	}
-	emailMessage := ToEmailMessageFromWorkflowExecutionEvent(notificationsConfig, emailNotification, request, model)
+	emailMessage := ToEmailMessageFromWorkflowExecutionEvent(notificationsConfig, emailNotification, request, workflowExecution)
 	assert.True(t, proto.Equal(emailMessage, &admin.EmailMessage{
 		RecipientsEmail: []string{
 			"a@example.com", "b@example.org",
