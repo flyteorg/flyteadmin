f1c78e2a8805d575b8f84e2dc8ffc69d42506206
Yee Hing Tong <ytong@lyft.com>
2019-10-31 08:46:14 -0700

    more comment

--- pkg/server/cors.go
+++ pkg/server/cors.go
@@ -5,10 +5,15 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/utilities"
 )
 
+const (
+	// At time of writing, the version argument is required to be 1.
+	GrpcGatewayPatternVersion = 1
+)
+
 // This is a function used for CORS support. It produces a Pattern object that can be attached to the grpc-gateway
 // ServeMux object. It should match any and all URLs. The two op codes say, push the entire path to the stack 'OpPushM',
 // and then ignore the result, 'OpNop'.
 func GetGlobPattern() runtime.Pattern {
-	return runtime.MustPattern(runtime.NewPattern(1, []int{int(utilities.OpPushM), int(utilities.OpNop)},
+	return runtime.MustPattern(runtime.NewPattern(GrpcGatewayPatternVersion, []int{int(utilities.OpPushM), int(utilities.OpNop)},
 		[]string{}, ""))
 }
