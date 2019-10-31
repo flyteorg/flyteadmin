d049c05a7ed15e612c957e529a1b4f9ef9204665
Yee Hing Tong <ytong@lyft.com>
2019-10-31 08:36:19 -0700

    update comment as per pr review

--- pkg/server/cors.go
+++ pkg/server/cors.go
@@ -6,8 +6,8 @@ import (
 )
 
 // This is a function used for CORS support. It produces a Pattern object that can be attached to the grpc-gateway
-// ServeMux object. It should match any and all URLs. The pattern says, push the entire path to the stack, and then
-// ignore the result.
+// ServeMux object. It should match any and all URLs. The two op codes say, push the entire path to the stack 'OpPushM',
+// and then ignore the result, 'OpNop'.
 func GetGlobPattern() runtime.Pattern {
 	return runtime.MustPattern(runtime.NewPattern(1, []int{int(utilities.OpPushM), int(utilities.OpNop)},
 		[]string{}, ""))
