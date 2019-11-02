3b893deddaf2d3f217b612efb5f14d05a25a7cd5
tnsetting <tnsetting@gmail.com>
2019-11-02 09:02:32 +0100

    Add test for case with pwd but no extra option

--- pkg/repositories/config/postgres_test.go
+++ pkg/repositories/config/postgres_test.go
@@ -32,3 +32,15 @@ func TestConstructGormArgsWithPassword(t *testing.T) {
 
 	assert.Equal(t, "host=localhost port=5432 dbname=postgres user=postgres password=pass sslmode=enable", postgresConfigProvider.GetArgs())
 }
+
+func TestConstructGormArgsWithPasswordNoExtra(t *testing.T) {
+	postgresConfigProvider := NewPostgresConfigProvider(DbConfig{
+		Host:     "localhost",
+		Port:     5432,
+		DbName:   "postgres",
+		User:     "postgres",
+		Password: "pass",
+	}, mockScope.NewTestScope())
+
+	assert.Equal(t, "host=localhost port=5432 dbname=postgres user=postgres password=pass ", postgresConfigProvider.GetArgs())
+}
