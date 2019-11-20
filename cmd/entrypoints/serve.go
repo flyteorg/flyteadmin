14425356f98274b41498b89475ee9aa70b77b3e6
Hongxin Liang <honnix@spotify.com>
2019-11-20 20:07:51 +0100

    add server reflection

--- cmd/entrypoints/serve.go
+++ cmd/entrypoints/serve.go
@@ -33,6 +33,7 @@ import (
 	"github.com/lyft/flytestdlib/contextutils"
 	"github.com/lyft/flytestdlib/promutils/labeled"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 // serveCmd represents the serve command
@@ -84,6 +85,7 @@ func newGRPCServer(ctx context.Context, cfg *config.ServerConfig, authContext in
 	grpcServer := grpc.NewServer(serverOpts...)
 	grpc_prometheus.Register(grpcServer)
 	flyteService.RegisterAdminServiceServer(grpcServer, adminservice.NewAdminServer(cfg.KubeConfig, cfg.Master))
+	reflection.Register(grpcServer)
 	return grpcServer, nil
 }
 
