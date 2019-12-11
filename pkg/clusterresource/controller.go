4494ee2d51d0232fdf451c38da2f6d4094403a73
Katrina Rogan <krogan@lyft.com>
2019-12-10 16:26:00 -0800

    wip

--- pkg/clusterresource/controller.go
+++ pkg/clusterresource/controller.go
@@ -285,13 +285,15 @@ func (c *controller) syncNamespace(ctx context.Context, namespace NamespaceName,
 		}
 		for _, target := range c.executionCluster.GetAllValidTargets() {
 			k8sObjCopy := k8sObj.DeepCopyObject()
+			logger.Debugf(ctx, "Attempting to create resource [%+v] in cluster [%v] for namespace [%s]",
+				k8sObj.GetObjectKind().GroupVersionKind().Kind, target.ID, namespace)
 			err = target.Client.Create(ctx, k8sObjCopy)
 			if err != nil {
 				if k8serrors.IsAlreadyExists(err) {
 					logger.Debugf(ctx, "Resource [%+v] in namespace [%s] already exists - attempting update instead",
 						k8sObj.GetObjectKind().GroupVersionKind().Kind, namespace)
 					c.metrics.AppliedTemplateExists.Inc()
-					err = target.Client.Patch(ctx, k8sObjCopy, client.MergeFrom(k8sObjCopy))
+					err = target.Client.Patch(ctx, k8sObjCopy, client.MergeFrom(k8sObj))
 					if err != nil {
 						c.metrics.TemplateUpdateErrors.Inc()
 						logger.Infof(ctx, "Failed to update resource [%+v] in namespace [%s] with err :%v",
