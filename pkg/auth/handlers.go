b56da868fc32369a90a875b03f8fb0375d2b49d9
Yee Hing Tong <ytong@lyft.com>
2019-12-12 17:15:08 -0800

    comments, placeholder replace

--- pkg/auth/handlers.go
+++ pkg/auth/handlers.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/grpc-ecosystem/go-grpc-middleware/util/metautils"
 	"net/http"
 
 	"github.com/gorilla/handlers"
-	grpcauth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"github.com/lyft/flyteadmin/pkg/auth/interfaces"
 	"github.com/lyft/flytestdlib/contextutils"
 	"github.com/lyft/flytestdlib/errors"
@@ -150,38 +150,35 @@ func GetAuthenticationCustomMetadataInterceptor(authCtx interfaces.Authenticatio
 	}
 }
 
-// This function will only look for a token from the request metadata, verify it, and extract the user email if valid.
-// Unless there is an error, it will not return an unauthorized status. That is up to subsequent functions to decide,
-// based on configuration.  We don't want to require authentication for all endpoints.
+// This is the function that chooses to enforce or not enforce authentication. It will attempt to get the token
+// from the incoming context, validate it, and decide whether or not to let the request through.
 func GetAuthenticationInterceptor(authContext interfaces.AuthenticationContext) func(context.Context) (context.Context, error) {
 	return func(ctx context.Context) (context.Context, error) {
 		logger.Debugf(ctx, "Running authentication gRPC interceptor")
-		tokenStr, err := grpcauth.AuthFromMD(ctx, BearerScheme)
-		if err != nil {
-			logger.Debugf(ctx, "Could not retrieve bearer token from metadata %v", err)
-			return ctx, nil
+
+		fromHTTP := metautils.ExtractIncoming(ctx).Get("from_http")
+		isFromHTTP := fromHTTP == "true"
+
+		token, err := GetAndValidateTokenObjectFromContext(ctx, authContext.Claims(), authContext.OidcProvider())
+
+		// Only enforcement logic is present. The default case is to let things through.
+		if (isFromHTTP && authContext.Options().EnforceHttp) ||
+			(!isFromHTTP && authContext.Options().EnforceGrpc) {
+			if err != nil {
+				return ctx, status.Errorf(codes.Unauthenticated, "token parse error %s", err)
+			}
+			if token == nil {
+				return ctx, status.Errorf(codes.Unauthenticated, "Token was nil after parsing")
+			} else if token.Subject == "" {
+				return ctx, status.Errorf(codes.Unauthenticated, "no email or empty email found")
+			}
 		}
 
-		// Currently auth is optional...
-		if tokenStr == "" {
-			logger.Debugf(ctx, "Bearer token is empty, skipping parsing")
-			return ctx, nil
-		}
-
-		// ...however, if there _is_ a bearer token, but there are additional errors downstream, then we return an
-		// authentication error.
-		token, err := ParseAndValidate(ctx, authContext.Claims(), tokenStr, authContext.OidcProvider())
-		if err != nil {
-			return ctx, status.Errorf(codes.Unauthenticated, "could not parse token string into object: %s %s", tokenStr, err)
-		}
-		if token == nil {
-			return ctx, status.Errorf(codes.Unauthenticated, "Token was nil after parsing %s", tokenStr)
-		} else if token.Subject == "" {
-			return ctx, status.Errorf(codes.Unauthenticated, "no email or empty email found")
-		} else {
-			newCtx := WithUserEmail(context.WithValue(ctx, bearerTokenContextKey, tokenStr), token.Subject)
+		if token != nil {
+			newCtx := WithUserEmail(context.WithValue(ctx, bearerTokenContextKey, token), token.Subject)
 			return newCtx, nil
 		}
+		return ctx, nil
 	}
 }
 
@@ -205,6 +202,16 @@ func GetHTTPRequestCookieToMetadataHandler(authContext interfaces.Authentication
 		}
 		return metadata.MD{
 			DefaultAuthorizationHeader: []string{fmt.Sprintf("%s %s", BearerScheme, accessToken)},
+		}
+	}
+}
+
+// Intercepts the incoming HTTP requests and marks it as such so that the downstream code can use it to enforce auth.
+// See the enforceHTTP/Grpc options for more information.
+func GetHTTPMetadataTaggingHandler(authContext interfaces.AuthenticationContext) HTTPRequestToMetadataAnnotator {
+	return func(ctx context.Context, request *http.Request) metadata.MD {
+		return metadata.MD{
+			"from_http": []string{"true"},
 		}
 	}
 }
