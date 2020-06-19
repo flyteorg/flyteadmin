6a8f7a95d419ded7b6a2fe2f67b67d4b7afcd021
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-06-19 09:07:04 -0700

    Fix script

--- script/integration/execute.sh
+++ script/integration/execute.sh
@@ -18,11 +18,26 @@ kind load docker-image flyteadmin:test
 # start flyteadmin and dependencies
 kubectl apply -f "${DIR}/k8s/integration.yaml"
 
-# wait for flyteadmin deployment to complete
-kubectl -n flyte rollout status deployment flyteadmin
+# This is a separate function so that we can potentially reuse in the future when we have more than one test
+function wait_for_flyte_deploys() {
+    SECONDS=0
+    echo ""
+    echo "waiting for flyte deploy to complete..."
+    echo ""
 
-# get the name of the flyteadmin pod
-POD_NAME=$(kubectl get pods -n flyte -o go-template="{{range .items}}{{.metadata.name}}:{{end}}" | tr ":" "\n" | grep flyteadmin | grep Running)
+    # wait for flyteadmin deployment to complete
+    kubectl -n flyte rollout status deployment flyteadmin
+    echo ""
+
+    echo "Flyte deployed in $SECONDS seconds."
+}
+
+wait_for_flyte_deploys
+
+## get the name of the flyteadmin pod
+POD_NAME=$(kubectl get pods -n flyte --field-selector=status.phase=Running -o go-template="{{range .items}}{{.metadata.name}}:{{end}}" | tr ":" "\n" | grep flyteadmin)
+
+echo $POD_NAME
 
 # launch the integration tests
 kubectl exec -it -n flyte "$POD_NAME" -- make -C /go/src/github.com/lyft/flyteadmin integration
