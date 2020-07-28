d19200baae1ddb3044c1698decd5c56ce1ebab8f
Ketan Umare <kumare@lyft.com>
2020-07-27 17:37:19 -0700

    Caching visibility support in FlyteAdmin

--- pkg/repositories/models/node_execution.go
+++ pkg/repositories/models/node_execution.go
@@ -36,5 +36,7 @@ type NodeExecution struct {
 	// Execution Error Kind. nullable, can be one of core.ExecutionError_ErrorKind
 	ErrorKind *string `gorm:"index"`
 	// Execution Error Code nullable. string value, but finite set determined by the execution engine and plugins
-	ErrorCode *string
+	ErrorCode   *string
+	// TODO, This is never really null after this point
+	CacheStatus *string
 }
