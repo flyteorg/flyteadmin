ec13c8d0819f592827ef36fe3cfd6c541c4838c8
Hongxin Liang <hxliang1982@gmail.com>
2020-11-24 23:44:29 +0100

    Remove redundant function

--- pkg/repositories/gormimpl/project_repo_test.go
+++ pkg/repositories/gormimpl/project_repo_test.go
@@ -63,41 +63,6 @@ func TestGetProject(t *testing.T) {
 	assert.Equal(t, int32(admin.Project_ACTIVE), *output.State)
 }
 
-func TestListAllProjects(t *testing.T) {
-	projectRepo := NewProjectRepo(GetDbForTest(t), errors.NewTestErrorTransformer(), mockScope.NewTestScope())
-	projects := make([]map[string]interface{}, 2)
-	fooProject := make(map[string]interface{})
-	fooProject["identifier"] = "foo"
-	fooProject["name"] = "foo =)"
-	fooProject["description"] = "foo description"
-	fooProject["state"] = admin.Project_ACTIVE
-	projects[0] = fooProject
-
-	barProject := make(map[string]interface{})
-	barProject["identifier"] = "bar"
-	barProject["name"] = "Bar"
-	barProject["description"] = "Bar description"
-	barProject["state"] = admin.Project_ACTIVE
-	projects[1] = barProject
-
-	GlobalMock := mocket.Catcher.Reset()
-	GlobalMock.Logging = true
-	GlobalMock.NewMock().WithQuery(`SELECT * FROM "projects"  WHERE "projects"."deleted_at" IS NULL AND ((state != 1)) ORDER BY identifier asc`).
-		WithReply(projects)
-
-	output, err := projectRepo.ListAll(context.Background(), alphabeticalSortParam)
-	assert.Nil(t, err)
-	assert.Len(t, output, 2)
-	assert.Equal(t, "foo", output[0].Identifier)
-	assert.Equal(t, "foo =)", output[0].Name)
-	assert.Equal(t, "foo description", output[0].Description)
-	assert.Equal(t, int32(admin.Project_ACTIVE), *output[0].State)
-	assert.Equal(t, "bar", output[1].Identifier)
-	assert.Equal(t, "Bar", output[1].Name)
-	assert.Equal(t, "Bar description", output[1].Description)
-	assert.Equal(t, int32(admin.Project_ACTIVE), *output[1].State)
-}
-
 func testListProjects(input interfaces.ListResourceInput, sql string, t *testing.T) {
 	projectRepo := NewProjectRepo(GetDbForTest(t), errors.NewTestErrorTransformer(), mockScope.NewTestScope())
 	projects := make([]map[string]interface{}, 1)
