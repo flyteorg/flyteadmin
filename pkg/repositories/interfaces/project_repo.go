78f455d7ce3a522f0cd6f528c4cee84b0eb94a88
Hongxin Liang <hxliang1982@gmail.com>
2020-12-01 08:50:21 +0100

    Fix wrong comment

--- pkg/repositories/interfaces/project_repo.go
+++ pkg/repositories/interfaces/project_repo.go
@@ -11,7 +11,7 @@ type ProjectRepoInterface interface {
 	Create(ctx context.Context, project models.Project) error
 	// Returns a matching project when it exists.
 	Get(ctx context.Context, projectID string) (models.Project, error)
-	// Returns projects matching query parameters. A limit must be provided for the results page size.
+	// Returns projects matching query parameters.
 	List(ctx context.Context, input ListResourceInput) ([]models.Project, error)
 	// Given a project that exists in the DB and a partial set of fields to update
 	// as a second project (projectUpdate), updates the original project which already
