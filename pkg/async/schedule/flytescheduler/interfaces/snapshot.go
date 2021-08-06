f09d6ca0b8324b04a5d26482ec6bee7560c4f360
Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
2021-08-06 13:13:08 +0530

    Using last updated timestamp for activation
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- pkg/async/schedule/flytescheduler/interfaces/snapshot.go
+++ pkg/async/schedule/flytescheduler/interfaces/snapshot.go
@@ -2,13 +2,20 @@ package interfaces
 
 import "time"
 
-// All threadsafe
-type Snapshot interface {
+// Snapshoter used by the scheduler for creating, updating and reading snapshots of the schedules.
+type Snapshoter interface {
+	// GetLastExecutionTime of the schedule given by the key
 	GetLastExecutionTime(key string) time.Time
+	// UpdateLastExecutionTime of the schedule given by key to the lastExecTime
 	UpdateLastExecutionTime(key string, lastExecTime time.Time)
+	// CreateSnapshot creates the snapshot of all the schedules and there execution times.
 	CreateSnapshot() ([]byte, error)
+	// BootstrapFrom bootstraps the snapshot from a byte array
 	BootstrapFrom(snapshot []byte) error
+	// GetVersion gets the version number of snapshot written
 	GetVersion() int
+	// IsEmpty returns true if the snapshot contains no schedules
 	IsEmpty() bool
-	AreEqual(snapshot Snapshot) bool
+	// AreEqual returns true if the prevSnapshot equals the schedules of the current snapshot
+	AreEqual(prevSnapshot Snapshoter) bool
 }
