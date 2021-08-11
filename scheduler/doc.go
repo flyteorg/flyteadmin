8e8c254d1bc5a278c8f99c63ec8d66405ba9cc44
Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
2021-08-11 20:51:09 +0530

    Updated the doc
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- scheduler/doc.go
+++ scheduler/doc.go
@@ -10,13 +10,19 @@
 //    Atmost one launchplan is active at any moment across its various versions and same semantics apply for the
 //    schedules aswell.
 // 2] Scheduler
-//    This component has its source in the current folder and is responsible for reading the schedules from the DB
-//    and running them at the cadence defined by there schedule
+//    This component is a singleton and has its source in the current folder and is responsible for reading the schedules
+//    from the DB and running them at the cadence defined by there schedule
 //    The lowest granularity supported is secs for scheduling through cron and minutes using the fixed rate scheduler
+// 	  The scheduler should be running in one replica , two at the most during redeployment. Multiple replicas will just
+// 	  duplicate the work since each execution for a scheduleTime will have unique identifier derived from schedule name
+//	  and time of the schedule. The idempotency aspect of the admin for same identifier prevents duplication on the admin
+//	  side.
+//    The scheduler runs continously in a loop reading the updated schedule entries in the data store and adding or removing
+//    the schedules. Removing a schedule doesn't gurantee about inflight routines launched by the scheduler.
 //    Sub components:
 //		a) Snapshoter
 // 			This component is responsible for writing the snapshot state of all the schedules at a regular cadence to a
-//			persistent store. The current implementation uses DB to store the GOB format of the snapshot
+//			persistent store. The current implementation uses DB to store the GOB format of the snapshot which is versioned.
 //			The snapshot is map[string]time.Time which stores a map of schedules names to there last execution times
 // 			During bootup the snapshot is bootstraped from the data store and loaded in memory
 //			The Scheduler use this snapshot to schedule any missed schedules.
@@ -35,5 +41,9 @@
 // 			schedules
 // 			The current implementation uses the lastExecTime from the snapshot to compute the next scheduled time to be
 //			sent for execution. This same time is then used as lastExecTime when the jobfunction is invoked again
+//		d) Job function :
+//			The job function accepts the scheduleTime and the schedule which is used for creating an execution request
+//			to the admin. Each job fuction is tied to schedule which gets executed in separate go routine by the gogf
+// 			framework in according the schedule cadence.
 
 package scheduler
