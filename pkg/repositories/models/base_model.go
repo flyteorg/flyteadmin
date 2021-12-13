3b2593347618d238b15c5e3258e88b2c6b2fef99
Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
2021-12-13 20:21:00 +0530

    Changed autoincrement tag
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- pkg/repositories/models/base_model.go
+++ pkg/repositories/models/base_model.go
@@ -6,7 +6,7 @@ import "time"
 // This is nearly identical to http://doc.gorm.io/models.html#conventions except that flyteadmin models define their
 // own primary keys rather than use the ID as the primary key
 type BaseModel struct {
-	ID        uint `gorm:"index;AUTO_INCREMENT"`
+	ID        uint `gorm:"index;autoIncrement:true"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time `sql:"index"`
