cbe487a2aba2e75872c2de7527952a46253ac1e6
Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
2021-12-14 16:45:57 +0530

    Converted ParentTaskExecutionID to *uint from unint
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- pkg/repositories/models/node_execution.go
+++ pkg/repositories/models/node_execution.go
@@ -36,7 +36,7 @@ type NodeExecution struct {
 	ChildNodeExecutions []NodeExecution `gorm:"foreignKey:ParentID;references:ID"`
 	// The task execution (if any) which launched this node execution.
 	// TO BE DEPRECATED - as we have now introduced ParentID
-	ParentTaskExecutionID uint `sql:"default:null" gorm:"index"`
+	ParentTaskExecutionID *uint `sql:"default:null" gorm:"index"`
 	// The workflow execution (if any) which this node execution launched
 	// NOTE: LaunchedExecution[foreignkey:ParentNodeExecutionID] refers to Workflow execution launched and is different from ParentID
 	LaunchedExecution Execution `gorm:"foreignKey:ParentNodeExecutionID;references:ID"`
