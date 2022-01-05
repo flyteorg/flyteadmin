ed313e64a90af82034b5fb1d6208ee52f3993c7a
Katrina Rogan <katroganGH@gmail.com>
2022-01-04 16:25:14 -0800

    Another unit test
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- pkg/repositories/transformers/execution_test.go
+++ pkg/repositories/transformers/execution_test.go
@@ -454,8 +454,9 @@ func TestSetExecutionAborted(t *testing.T) {
 			}},
 		// The execution abort metadata is recorded but the phase is not actually updated *until* the abort event is
 		// propagated by flytepropeller.
-		Phase: core.WorkflowExecution_RUNNING,
+		Phase: core.WorkflowExecution_ABORTING,
 	}, &actualClosure))
+	assert.Equal(t, existingModel.AbortCause, cause)
 }
 
 func TestGetExecutionIdentifier(t *testing.T) {
