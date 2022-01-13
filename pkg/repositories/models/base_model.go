a5f1ba2b6cfa833bf3ebde516236ff99a53779fd
Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
2022-01-13 18:44:15 +0530

    Removed the bigint type conversion on ID column and instead using only during migration
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- pkg/repositories/models/base_model.go
+++ pkg/repositories/models/base_model.go
@@ -6,7 +6,7 @@ import "time"
 // This is nearly identical to http://doc.gorm.io/models.html#conventions except that flyteadmin models define their
 // own primary keys rather than use the ID as the primary key
 type BaseModel struct {
-	ID        uint `gorm:"type:bigint;index;autoIncrement"`
+	ID        uint `gorm:"index;autoIncrement"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time `gorm:"index"`
