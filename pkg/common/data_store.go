8595b03a3d08af2f0899f77d950fa6db07ca3aa7
Sonja Ericsson <sonjae@spotify.com>
2022-01-19 15:24:59 +0100

    fix
    Signed-off-by: Sonja Ericsson <sonjae@spotify.com>

--- pkg/common/data_store.go
+++ pkg/common/data_store.go
@@ -2,13 +2,15 @@ package common
 
 import (
 	"context"
-
+	"github.com/flyteorg/flyteadmin/pkg/async"
 	"github.com/flyteorg/flyteadmin/pkg/errors"
 	"github.com/flyteorg/flyteadmin/pkg/manager/impl/shared"
-	"google.golang.org/grpc/codes"
-
 	"github.com/flyteorg/flyteidl/gen/pb-go/flyteidl/core"
+	"github.com/flyteorg/flytestdlib/logger"
 	"github.com/flyteorg/flytestdlib/storage"
+	"google.golang.org/api/googleapi"
+	"google.golang.org/grpc/codes"
+	"time"
 )
 
 func OffloadLiteralMap(ctx context.Context, storageClient *storage.DataStore, literalMap *core.LiteralMap, nestedKeys ...string) (storage.DataReference, error) {
@@ -32,7 +34,7 @@ func OffloadLiteralMap(ctx context.Context, storageClient *storage.DataStore, li
 	Error 409: The metadata for object "metadata/payment-risk-datasets/production/cz7vntanluzg2fck4yim/inputs" was edited during the operation.
 	Please try again., conflict", "ts":"2021-12-27T05:33:19Z"}
 	*/
-	err = async.Retry(5, async.RetryDelay, func() error {
+	err = Retry(5, async.RetryDelay, func() error {
 		err = storageClient.WriteProtobuf(ctx, uri, storage.Options{}, literalMap)
 		return err
 	})
@@ -42,4 +44,22 @@ func OffloadLiteralMap(ctx context.Context, storageClient *storage.DataStore, li
 	}
 
 	return uri, nil
+}
+
+func Retry(attempts int, delay time.Duration, f func() error) error {
+	var err error
+	for attempt := 0; attempt <= attempts; attempt++ {
+		err = f()
+		if err == nil {
+			return nil
+		}
+		if e, ok := err.(*googleapi.Error); ok && e.Code == 409 {
+			logger.Warningf(context.Background(),
+				"Failed [%v] on attempt %d of %d", err, attempt, attempts)
+			time.Sleep(delay)
+		} else {
+			return err
+		}
+	}
+	return err
 }
