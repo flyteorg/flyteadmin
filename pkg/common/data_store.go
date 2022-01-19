5f2dc965aefd872e0714c600012475f7ad61ac1c
Sonja Ericsson <sonjae@spotify.com>
2022-01-19 15:59:17 +0100

    fix
    Signed-off-by: Sonja Ericsson <sonjae@spotify.com>

--- pkg/common/data_store.go
+++ pkg/common/data_store.go
@@ -6,11 +6,9 @@ import (
 	"github.com/flyteorg/flyteadmin/pkg/errors"
 	"github.com/flyteorg/flyteadmin/pkg/manager/impl/shared"
 	"github.com/flyteorg/flyteidl/gen/pb-go/flyteidl/core"
-	"github.com/flyteorg/flytestdlib/logger"
 	"github.com/flyteorg/flytestdlib/storage"
 	"google.golang.org/api/googleapi"
 	"google.golang.org/grpc/codes"
-	"time"
 )
 
 func OffloadLiteralMap(ctx context.Context, storageClient *storage.DataStore, literalMap *core.LiteralMap, nestedKeys ...string) (storage.DataReference, error) {
@@ -26,10 +24,10 @@ func OffloadLiteralMap(ctx context.Context, storageClient *storage.DataStore, li
 		return "", errors.NewFlyteAdminErrorf(codes.Internal, "Failed to construct data reference for [%+v] with err: %v", nestedKeys, err)
 	}
 
-	err = Retry(5, async.RetryDelay, func() error {
+	err = async.RetryOnSpecificErrors(5, async.RetryDelay, func() error {
 		err = storageClient.WriteProtobuf(ctx, uri, storage.Options{}, literalMap)
 		return err
-	})
+	}, isRetryableError)
 
 	if err != nil {
 		return "", errors.NewFlyteAdminErrorf(codes.Internal, "Failed to write protobuf for [%+v] with err: %v", nestedKeys, err)
@@ -38,20 +36,9 @@ func OffloadLiteralMap(ctx context.Context, storageClient *storage.DataStore, li
 	return uri, nil
 }
 
-func Retry(attempts int, delay time.Duration, f func() error) error {
-	var err error
-	for attempt := 0; attempt <= attempts; attempt++ {
-		err = f()
-		if err == nil {
-			return nil
-		}
-		if e, ok := err.(*googleapi.Error); ok && e.Code == 409 {
-			logger.Warningf(context.Background(),
-				"Failed [%v] on attempt %d of %d", err, attempt, attempts)
-			time.Sleep(delay)
-		} else {
-			return err
-		}
+func isRetryableError(err error) bool {
+	if e, ok := err.(*googleapi.Error); ok && e.Code == 409 {
+		return true
 	}
-	return err
+	return false
 }
