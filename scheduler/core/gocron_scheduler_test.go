87b1a65a7f99269fcc403bc84e9c0693da3a4e88
Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>
2022-01-28 11:34:14 +0530

    Linter fixes
    Signed-off-by: Prafulla Mahindrakar <prafulla.mahindrakar@gmail.com>

--- scheduler/core/gocron_scheduler_test.go
+++ scheduler/core/gocron_scheduler_test.go
@@ -1,10 +1,11 @@
 package core
 
 import (
-	"github.com/flyteorg/flyteadmin/scheduler/repositories/models"
-	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+
+	"github.com/flyteorg/flyteadmin/scheduler/repositories/models"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetCronScheduledTime(t *testing.T) {
@@ -19,7 +20,6 @@ func TestGetCatchUpTimes(t *testing.T) {
 	t.Run("to time before scheduled time", func(t *testing.T) {
 		s := models.SchedulableEntity{
 			CronExpression: "0 19 * * *",
-
 		}
 		from := time.Date(2022, time.January, 27, 19, 0, 0, 0, time.UTC)
 		to := time.Date(2022, time.January, 28, 00, 51, 6, 0, time.UTC)
@@ -30,7 +30,6 @@ func TestGetCatchUpTimes(t *testing.T) {
 	t.Run("to time equal scheduled time", func(t *testing.T) {
 		s := models.SchedulableEntity{
 			CronExpression: "0 19 * * *",
-
 		}
 		from := time.Date(2022, time.January, 27, 19, 0, 0, 0, time.UTC)
 		to := time.Date(2022, time.January, 28, 19, 0, 0, 0, time.UTC)
@@ -44,7 +43,6 @@ func TestGetCatchUpTimes(t *testing.T) {
 	t.Run("to time after scheduled time", func(t *testing.T) {
 		s := models.SchedulableEntity{
 			CronExpression: "0 19 * * *",
-
 		}
 		from := time.Date(2022, time.January, 27, 19, 0, 0, 0, time.UTC)
 		to := time.Date(2022, time.January, 28, 19, 50, 0, 0, time.UTC)
