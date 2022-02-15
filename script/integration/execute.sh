941f69aaf7d6f7ad464a8481879ba7b4379090aa
Katrina Rogan <katroganGH@gmail.com>
2022-02-15 13:07:00 -0800

    big wow
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- script/integration/execute.sh
+++ script/integration/execute.sh
@@ -23,11 +23,6 @@ function wait_for_flyte_deploys() {
     echo "waiting for flyte deploy to complete..."
     echo ""
 
-    kubectl -n flyte get deployment flyteadmin -o yaml
-    kubectl -n flyte describe deploy flyteadmin
-    kubectl -n flyte describe service flyteadmin
-    kubectl -n flyte get configmap flyte-admin-config   -o yaml
-
     # wait for flyteadmin deployment to complete
     kubectl -n flyte rollout status deployment flyteadmin
     echo ""
