f2ec16e525204b30a8c11d8d2f1d16c9c66c989b
Kevin Su <pingsutw@apache.org>
2022-03-27 02:23:47 +0800

    Read database config from datacatalog when failed to register config
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- go.mod
+++ go.mod
@@ -84,6 +84,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/flyteorg/datacatalog v0.3.26
 	github.com/form3tech-oss/jwt-go v3.2.2+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-logr/logr v0.4.0 // indirect
@@ -107,7 +108,7 @@ require (
 	github.com/jackc/pgtype v1.9.0 // indirect
 	github.com/jackc/pgx/v4 v4.14.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.3 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0 // indirect
@@ -170,3 +171,5 @@ require (
 )
 
 replace github.com/robfig/cron/v3 => github.com/unionai/cron/v3 v3.0.2-0.20210825070134-bfc34418fe84
+
+replace github.com/flyteorg/datacatalog => /Users/kevin/git/datacatalog
--- pkg/runtime/application_config_provider.go
+++ pkg/runtime/application_config_provider.go
@@ -3,6 +3,7 @@ package runtime
 import (
 	"time"
 
+	datacatalogDbConfig "github.com/flyteorg/datacatalog/pkg/repositories/config"
 	"github.com/flyteorg/flyteadmin/pkg/common"
 	"github.com/flyteorg/flyteadmin/pkg/runtime/interfaces"
 	"github.com/flyteorg/flytestdlib/config"
@@ -21,7 +22,7 @@ const postgres = "postgres"
 const KB = 1024
 const MB = KB * KB
 
-var databaseConfig = config.MustRegisterSection(database, &interfaces.DbConfig{
+var databaseConfig, err = config.RegisterSection(database, &interfaces.DbConfig{
 	DeprecatedPort:         5432,
 	DeprecatedUser:         postgres,
 	DeprecatedHost:         postgres,
@@ -90,6 +91,47 @@ var externalEventsConfig = config.MustRegisterSection(externalEvents, &interface
 type ApplicationConfigurationProvider struct{}
 
 func (p *ApplicationConfigurationProvider) GetDbConfig() *interfaces.DbConfig {
+	// The error only occurs when using single binary to launch flyte cluster.
+	// The reason is that we try to register "database" section in flyteadmin and datacatalog
+	// Here we will read database config from datacatalog if it exists.
+	if err != nil {
+		databaseSection := config.GetSection(database)
+		datacatalogDatabaseConfig := databaseSection.GetConfig().(*datacatalogDbConfig.DbConfigSection)
+		switch {
+		case len(datacatalogDatabaseConfig.Host) > 0 || len(datacatalogDatabaseConfig.User) > 0 || len(datacatalogDatabaseConfig.DbName) > 0:
+			return &interfaces.DbConfig{
+				DeprecatedPort:     datacatalogDatabaseConfig.Port,
+				DeprecatedUser:     datacatalogDatabaseConfig.User,
+				DeprecatedHost:     datacatalogDatabaseConfig.Host,
+				DeprecatedDbName:   datacatalogDatabaseConfig.DbName,
+				MaxIdleConnections: 10,
+				MaxOpenConnections: 1000,
+				ConnMaxLifeTime:    config.Duration{Duration: time.Hour},
+			}
+		case datacatalogDatabaseConfig.PostgresConfig != nil:
+			return &interfaces.DbConfig{
+				PostgresConfig: &interfaces.PostgresConfig{
+					Host:         datacatalogDatabaseConfig.PostgresConfig.Host,
+					Port:         datacatalogDatabaseConfig.PostgresConfig.Port,
+					DbName:       datacatalogDatabaseConfig.PostgresConfig.DbName,
+					User:         datacatalogDatabaseConfig.PostgresConfig.User,
+					Password:     datacatalogDatabaseConfig.PostgresConfig.Password,
+					PasswordPath: datacatalogDatabaseConfig.PostgresConfig.PasswordPath,
+					ExtraOptions: datacatalogDatabaseConfig.PostgresConfig.ExtraOptions,
+					Debug:        datacatalogDatabaseConfig.PostgresConfig.Debug,
+				},
+			}
+		case datacatalogDatabaseConfig.SQLiteConfig != nil:
+			return &interfaces.DbConfig{
+				SQLiteConfig: &interfaces.SQLiteConfig{
+					File: datacatalogDatabaseConfig.SQLiteConfig.File,
+				},
+			}
+		default:
+			return &interfaces.DbConfig{}
+		}
+	}
+
 	return databaseConfig.GetConfig().(*interfaces.DbConfig)
 }
 
