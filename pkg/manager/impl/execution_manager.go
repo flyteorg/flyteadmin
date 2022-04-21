324dd2af69db61c876c79ab5065f2acf27867e7a
Katrina Rogan <katroganGH@gmail.com>
2022-04-21 11:59:28 -0700

    Keep populating execution config attributes from fallbacks
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- pkg/manager/impl/execution_manager.go
+++ pkg/manager/impl/execution_manager.go
@@ -450,11 +450,9 @@ type WorkflowExecutionConfigInterface interface {
 }
 
 // Merge into workflowExecConfig from spec and return true if any value has been changed
-func mergeIntoExecConfig(workflowExecConfig *admin.WorkflowExecutionConfig, spec WorkflowExecutionConfigInterface) bool {
-	isChanged := false
+func mergeIntoExecConfig(workflowExecConfig admin.WorkflowExecutionConfig, spec WorkflowExecutionConfigInterface) admin.WorkflowExecutionConfig {
 	if workflowExecConfig.GetMaxParallelism() == 0 && spec.GetMaxParallelism() > 0 {
 		workflowExecConfig.MaxParallelism = spec.GetMaxParallelism()
-		isChanged = true
 	}
 
 	if workflowExecConfig.GetSecurityContext() == nil && spec.GetSecurityContext() != nil {
@@ -462,7 +460,6 @@ func mergeIntoExecConfig(workflowExecConfig *admin.WorkflowExecutionConfig, spec
 			(len(spec.GetSecurityContext().GetRunAs().GetK8SServiceAccount()) > 0 ||
 				len(spec.GetSecurityContext().GetRunAs().GetIamRole()) > 0) {
 			workflowExecConfig.SecurityContext = spec.GetSecurityContext()
-			isChanged = true
 		}
 	}
 	// Launchplan spec has label, annotation and rawOutputDataConfig initialized with empty values.
@@ -471,19 +468,16 @@ func mergeIntoExecConfig(workflowExecConfig *admin.WorkflowExecutionConfig, spec
 		len(workflowExecConfig.GetRawOutputDataConfig().GetOutputLocationPrefix()) == 0) &&
 		(spec.GetRawOutputDataConfig() != nil && len(spec.GetRawOutputDataConfig().OutputLocationPrefix) > 0) {
 		workflowExecConfig.RawOutputDataConfig = spec.GetRawOutputDataConfig()
-		isChanged = true
 	}
 	if (workflowExecConfig.GetLabels() == nil || len(workflowExecConfig.GetLabels().Values) == 0) &&
 		(spec.GetLabels() != nil && len(spec.GetLabels().Values) > 0) {
 		workflowExecConfig.Labels = spec.GetLabels()
-		isChanged = true
 	}
 	if (workflowExecConfig.GetAnnotations() == nil || len(workflowExecConfig.GetAnnotations().Values) == 0) &&
 		(spec.GetAnnotations() != nil && len(spec.GetAnnotations().Values) > 0) {
 		workflowExecConfig.Annotations = spec.GetAnnotations()
-		isChanged = true
 	}
-	return isChanged
+	return workflowExecConfig
 }
 
 // Produces execution-time attributes for workflow execution.
@@ -493,20 +487,14 @@ func mergeIntoExecConfig(workflowExecConfig *admin.WorkflowExecutionConfig, spec
 func (m *ExecutionManager) getExecutionConfig(ctx context.Context, request *admin.ExecutionCreateRequest,
 	launchPlan *admin.LaunchPlan) (*admin.WorkflowExecutionConfig, error) {
 
-	workflowExecConfig := &admin.WorkflowExecutionConfig{}
+	workflowExecConfig := admin.WorkflowExecutionConfig{}
 	// merge the request spec into workflowExecConfig
-	if isChanged := mergeIntoExecConfig(workflowExecConfig, request.Spec); isChanged {
-		logger.Infof(ctx, "getting the workflow execution config from request spec")
-		return workflowExecConfig, nil
-	}
+	workflowExecConfig = mergeIntoExecConfig(workflowExecConfig, request.Spec)
 
 	var workflowName string
 	if launchPlan != nil && launchPlan.Spec != nil {
 		// merge the launch plan spec into workflowExecConfig
-		if isChanged := mergeIntoExecConfig(workflowExecConfig, launchPlan.Spec); isChanged {
-			logger.Infof(ctx, "getting the workflow execution config from launchplan spec")
-			return workflowExecConfig, nil
-		}
+		workflowExecConfig = mergeIntoExecConfig(workflowExecConfig, launchPlan.Spec)
 		if launchPlan.Spec.WorkflowId != nil {
 			workflowName = launchPlan.Spec.WorkflowId.Name
 		}
@@ -520,17 +508,14 @@ func (m *ExecutionManager) getExecutionConfig(ctx context.Context, request *admi
 
 	if matchableResource != nil && matchableResource.Attributes.GetWorkflowExecutionConfig() != nil {
 		// merge the matchable resource workflow execution config into workflowExecConfig
-		if isChanged := mergeIntoExecConfig(workflowExecConfig,
-			matchableResource.Attributes.GetWorkflowExecutionConfig()); isChanged {
-			logger.Infof(ctx, "getting the workflow execution config from workflow execution matchable attribute")
-			return workflowExecConfig, nil
-		}
+		workflowExecConfig = mergeIntoExecConfig(workflowExecConfig,
+			matchableResource.Attributes.GetWorkflowExecutionConfig())
 	}
 	//  merge the application config into workflowExecConfig
-	mergeIntoExecConfig(workflowExecConfig, m.config.ApplicationConfiguration().GetTopLevelConfig())
+	workflowExecConfig = mergeIntoExecConfig(workflowExecConfig, m.config.ApplicationConfiguration().GetTopLevelConfig())
 	logger.Infof(ctx, "getting the workflow execution config from application configuration")
 	// Defaults to one from the application config
-	return workflowExecConfig, nil
+	return &workflowExecConfig, nil
 }
 
 func (m *ExecutionManager) getClusterAssignment(ctx context.Context, request *admin.ExecutionCreateRequest) (
