0eab3995f435b126b9943f87d6c11846a536cd50
Katrina Rogan <katroganGH@gmail.com>
2022-05-10 15:16:05 -0700

    one more time, with feeling
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- tests/bootstrap.go
+++ tests/bootstrap.go
@@ -6,6 +6,7 @@ package tests
 import (
 	"context"
 	"fmt"
+	"github.com/flyteorg/flytestdlib/database"
 
 	"github.com/flyteorg/flyteadmin/pkg/repositories"
 	runtimeInterfaces "github.com/flyteorg/flyteadmin/pkg/runtime/interfaces"
@@ -22,9 +23,9 @@ const insertExecutionQueryStr = `INSERT INTO "executions" ` +
 
 var adminScope = promutils.NewScope("flyteadmin")
 
-func getDbConfig() *runtimeInterfaces.DbConfig {
-	return &runtimeInterfaces.DbConfig{
-		PostgresConfig: &runtimeInterfaces.PostgresConfig{
+func getDbConfig() *database.DbConfig {
+	return &database.DbConfig{
+		Postgres: database.PostgresConfig{
 			Host:   "postgres",
 			Port:   5432,
 			DbName: "postgres",
