060a5d0b437e980c876f5c7d9333798ce3509aed
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2023-02-27 09:51:15 -0800

    empty structs for task resrouce defaults
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- pkg/runtime/task_resource_provider.go
+++ pkg/runtime/task_resource_provider.go
@@ -3,21 +3,13 @@ package runtime
 import (
 	"github.com/flyteorg/flyteadmin/pkg/runtime/interfaces"
 	"github.com/flyteorg/flytestdlib/config"
-	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 const taskResourceKey = "task_resources"
 
 var taskResourceConfig = config.MustRegisterSection(taskResourceKey, &TaskResourceSpec{
-	Defaults: interfaces.TaskResourceSet{
-		CPU:    resource.MustParse("2"),
-		Memory: resource.MustParse("200Mi"),
-	},
-	Limits: interfaces.TaskResourceSet{
-		CPU:    resource.MustParse("2"),
-		Memory: resource.MustParse("1Gi"),
-		GPU:    resource.MustParse("1"),
-	},
+	Defaults: interfaces.TaskResourceSet{},
+	Limits:   interfaces.TaskResourceSet{},
 })
 
 type TaskResourceSpec struct {
