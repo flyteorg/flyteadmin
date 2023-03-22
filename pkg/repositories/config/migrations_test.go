873b37729e57878a5856fa11a5b874d4a742c5ec
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2023-03-21 17:14:00 -0700

    cleanup
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- pkg/repositories/config/migrations_test.go
+++ pkg/repositories/config/migrations_test.go
@@ -1,18 +1,9 @@
 package config
 
 import (
-	"context"
-	"fmt"
-	"github.com/flyteorg/flytestdlib/database"
-	"github.com/go-gormigrate/gormigrate/v2"
-	gormLogger "gorm.io/gorm/logger"
-	"log"
-	"os"
-	"testing"
-	"time"
-
 	mocket "github.com/Selvatico/go-mocket"
 	"github.com/stretchr/testify/assert"
+	"testing"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -39,119 +30,4 @@ func GetDbForTest(t *testing.T) *gorm.DB {
 		t.Fatalf("Failed to open mock db with err %v", err)
 	}
 	return db
-}
-
-func TestNoopMigrations(t *testing.T) {
-	gLogger := gormLogger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), gormLogger.Config{
-		SlowThreshold:             200 * time.Millisecond,
-		LogLevel:                  gormLogger.Info,
-		IgnoreRecordNotFoundError: false,
-		Colorful:                  true,
-	})
-
-	gormConfig := &gorm.Config{
-		Logger:                                   gLogger,
-		DisableForeignKeyConstraintWhenMigrating: false,
-	}
-
-	var gormDb *gorm.DB
-	pgConfig := database.PostgresConfig{
-		Host:         "localhost",
-		Port:         30001,
-		DbName:       "migratecopy1",
-		User:         "postgres",
-		Password:     "postgres",
-		ExtraOptions: "",
-		Debug:        false,
-	}
-	ctx := context.Background()
-	postgresDsn := database.PostgresDsn(ctx, pgConfig)
-	dialector := postgres.Open(postgresDsn)
-	gormDb, err := gorm.Open(dialector, gormConfig)
-	assert.NoError(t, err)
-
-	fmt.Println(gormDb)
-
-	m := gormigrate.New(gormDb, gormigrate.DefaultOptions, Migrations)
-	if err := m.Migrate(); err != nil {
-		fmt.Errorf("database migration failed: %v", err)
-	}
-	fmt.Println(ctx, "Migration ran successfully")
-}
-
-// Before running, create database replicator;
-func TestMigrationReplicate(t *testing.T) {
-	gLogger := gormLogger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), gormLogger.Config{
-		SlowThreshold:             200 * time.Millisecond,
-		LogLevel:                  gormLogger.Info,
-		IgnoreRecordNotFoundError: false,
-		Colorful:                  true,
-	})
-
-	gormConfig := &gorm.Config{
-		Logger:                                   gLogger,
-		DisableForeignKeyConstraintWhenMigrating: true,
-	}
-
-	var gormDb *gorm.DB
-	pgConfig := database.PostgresConfig{
-		Host:         "localhost",
-		Port:         30001,
-		DbName:       "replicator2", // this should be a completely blank database
-		User:         "postgres",
-		Password:     "postgres",
-		ExtraOptions: "",
-		Debug:        false,
-	}
-	ctx := context.Background()
-	postgresDsn := database.PostgresDsn(ctx, pgConfig)
-	dialector := postgres.Open(postgresDsn)
-	gormDb, err := gorm.Open(dialector, gormConfig)
-	assert.NoError(t, err)
-
-	fmt.Println(gormDb)
-
-	m := gormigrate.New(gormDb, gormigrate.DefaultOptions, NoopMigrations)
-	if err := m.Migrate(); err != nil {
-		fmt.Errorf("database migration failed: %v", err)
-	}
-	fmt.Println(ctx, "Migration ran successfully")
-}
-
-func TestLegacyOnlyMigrations(t *testing.T) {
-	gLogger := gormLogger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), gormLogger.Config{
-		SlowThreshold:             200 * time.Millisecond,
-		LogLevel:                  gormLogger.Info,
-		IgnoreRecordNotFoundError: false,
-		Colorful:                  true,
-	})
-
-	gormConfig := &gorm.Config{
-		Logger:                                   gLogger,
-		DisableForeignKeyConstraintWhenMigrating: true,
-	}
-
-	var gormDb *gorm.DB
-	pgConfig := database.PostgresConfig{
-		Host:         "localhost",
-		Port:         30001,
-		DbName:       "currentempty",
-		User:         "postgres",
-		Password:     "postgres",
-		ExtraOptions: "",
-		Debug:        false,
-	}
-	ctx := context.Background()
-	postgresDsn := database.PostgresDsn(ctx, pgConfig)
-	dialector := postgres.Open(postgresDsn)
-	gormDb, err := gorm.Open(dialector, gormConfig)
-	assert.NoError(t, err)
-
-	fmt.Println(gormDb)
-
-	m := gormigrate.New(gormDb, gormigrate.DefaultOptions, LegacyMigrations)
-	if err := m.Migrate(); err != nil {
-		fmt.Errorf("database migration failed: %v", err)
-	}
-	fmt.Println(ctx, "Migration ran successfully")
 }
