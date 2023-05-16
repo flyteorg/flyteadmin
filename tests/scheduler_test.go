99449f8d443e493df25673f519e5fece666c3574
eduardo apolinario <eapolinario@users.noreply.github.com>
2023-05-15 17:53:41 -0700

    Fix flaky test
    Signed-off-by: eduardo apolinario <eapolinario@users.noreply.github.com>

--- tests/scheduler_test.go
+++ tests/scheduler_test.go
@@ -1,6 +1,3 @@
-//go:build integration
-// +build integration
-
 package tests
 
 import (
@@ -59,15 +56,20 @@ func TestScheduleJob(t *testing.T) {
 		lastExecTime  *time.Time
 		assertionFunc func(t assert.TestingT, expected, actual interface{}, msgAndArgs ...interface{}) bool
 	}{
-		{testName: "using_schedule_time", lastExecTime: &now, assertionFunc: assert.Equal},
-		{testName: "without_schedule_time", lastExecTime: nil, assertionFunc: assert.NotEqual},
+		{testName: "without_schedule_time", lastExecTime: nil},
+		{testName: "using_schedule_time", lastExecTime: &now},
 	}
 	wg := &sync.WaitGroup{}
 	for _, tc := range tests {
+		tc := tc
 		t.Run(tc.testName, func(t *testing.T) {
 			wg.Add(1)
 			timedFuncWithSchedule := func(jobCtx context.Context, schedule models.SchedulableEntity, scheduleTime time.Time) error {
-				tc.assertionFunc(t, now, scheduleTime)
+				if scheduleTime.IsZero() {
+					assert.NotEqual(t, now, scheduleTime)
+				} else {
+					assert.WithinDuration(t, now, scheduleTime, time.Minute*2)
+				}
 				wg.Done()
 				return nil
 			}
