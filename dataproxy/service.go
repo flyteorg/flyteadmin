5f85815697730f686d13f56d739e49d31f27c322
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2023-06-19 09:35:43 -0700

    save a variable
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- dataproxy/service.go
+++ dataproxy/service.go
@@ -77,13 +77,13 @@ func (s Service) CreateUploadLocation(ctx context.Context, req *service.CreateUp
 	}
 
 	md5 := base64.StdEncoding.EncodeToString(req.ContentMd5)
-	urlSafeMd5 := base32.StdEncoding.EncodeToString(req.ContentMd5)
 
 	var prefix string
 	if req.FilenameRoot != "" {
 		prefix = req.FilenameRoot
 	} else {
-		prefix = urlSafeMd5
+		// url safe base32 encoding
+		prefix = base32.StdEncoding.EncodeToString(req.ContentMd5)
 	}
 	storagePath, err := createStorageLocation(ctx, s.dataStore, s.cfg.Upload,
 		req.Project, req.Domain, prefix, req.Filename)
