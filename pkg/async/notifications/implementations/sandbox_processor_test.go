c2a9301bb8bfcf15485474725a87875358178e33
Future Outlier <eric901201@gmai.com>
2023-08-09 11:01:41 +0800

    add email error for sandbox processor test, trying to imporove the code coverage
    Signed-off-by: Future Outlier <eric901201@gmai.com>

--- pkg/async/notifications/implementations/sandbox_processor_test.go
+++ pkg/async/notifications/implementations/sandbox_processor_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/flyteorg/flyteadmin/pkg/async/notifications/mocks"
 	"github.com/flyteorg/flyteidl/gen/pb-go/flyteidl/admin"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,6 +14,7 @@ var mockSandboxEmailer mocks.MockEmailer
 
 func TestSandboxProcessor_StartProcessing(t *testing.T) {
 	msgChan := make(chan []byte, 1)
+	msgChan <- msg
 	testSandboxProcessor := NewSandboxProcessor(msgChan, &mockSandboxEmailer)
 
 	sendEmailValidationFunc := func(ctx context.Context, email admin.EmailMessage) error {
@@ -22,6 +24,27 @@ func TestSandboxProcessor_StartProcessing(t *testing.T) {
 		assert.Equal(t, testEmail.SenderEmail, email.SenderEmail)
 		return nil
 	}
+
 	mockSandboxEmailer.SetSendEmailFunc(sendEmailValidationFunc)
 	assert.Nil(t, testSandboxProcessor.(*SandboxProcessor).run())
+}
+
+func TestSandboxProcessor_StartProcessingEmailError(t *testing.T) {
+	msgChan := make(chan []byte, 1)
+	msgChan <- msg
+	testSandboxProcessor := NewSandboxProcessor(msgChan, &mockSandboxEmailer)
+
+	emailError := errors.New("error sending email")
+	sendEmailValidationFunc := func(ctx context.Context, email admin.EmailMessage) error {
+		return emailError
+	}
+
+	mockSandboxEmailer.SetSendEmailFunc(sendEmailValidationFunc)
+	assert.NotNil(t, testSandboxProcessor.(*SandboxProcessor).run())
+}
+
+func TestSandboxProcessor_StopProcessing(t *testing.T) {
+	msgChan := make(chan []byte, 1)
+	testSandboxProcessor := NewSandboxProcessor(msgChan, &mockSandboxEmailer)
+	assert.Nil(t, testSandboxProcessor.StopProcessing())
 }
